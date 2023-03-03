969eb9d5169c1d0021d168c3a37fcacd3c2ed74a
VinceMalesys <vince.malesys@student.vives.be>
2023-03-03 14:39:04 +0100

    final commit

---
+++ Semester 2/Labo 2.3/opdracht colorpicker/scripts/code.js
@@ -0,0 +1,31 @@
+
+const setup = () => {
+	let colorDemos=document.getElementsByClassName("colorDemo");
+	let sliders = document.getElementsByClassName("slider");
+
+
+	for (let i = 0; i < sliders.length; i++) {
+		sliders[i].addEventListener("change", update);
+		sliders[i].addEventListener("input", update);
+	}
+
+	colorDemos[0].style.backgroundColor = 'rgb(' + 100 + ',' + 100 + ',' + 100 + ')';
+}
+const update = () => {
+	let colorDemos=document.getElementsByClassName("colorDemo");
+	let sliders = document.getElementsByClassName("slider");
+	let paragrafen = document.getElementsByTagName("p");
+	let value=sliders[0].value;
+	let value1 = sliders[1].value;
+	let value2 = sliders[2].value;
+	paragrafen[0].innerHTML = "Red " + value;
+	paragrafen[1].innerHTML = "Green " + value1;
+	paragrafen[2].innerHTML = "Blue " + value2;
+
+
+	colorDemos[0].style.backgroundColor = 'rgb(' + value + ',' + value1 + ',' + value2 + ')';
+
+}
+
+
+window.addEventListener('load', setup);
---
+++ Semester 2/Opdracht Leeg project/scripts/index.js
@@ -0,0 +1,10 @@
+const setup = () => {
+
+        let p = document.getElementsByTagName("p");
+        p[0].innerHTML = "gelukt";
+
+
+
+}
+
+window.addEventListener('load', setup);
