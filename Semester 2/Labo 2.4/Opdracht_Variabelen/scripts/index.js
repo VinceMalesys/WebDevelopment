ba11b10afddd79df54258dc32c9d677b4a1b948d
VinceMalesys <vince.malesys@student.vives.be>
2023-03-08 20:29:35 +0100

    final commit

--- Semester 2/Labo 2.4/Opdracht_Variabelen/scripts/index.js
+++ Semester 2/Labo 2.4/Opdracht_Variabelen/scripts/index.js
@@ -1,23 +1,6 @@
 const setup = () => {
 
-    window.addEventListener('click', test);
 
-
-}
-const test = () => {
-
-    let text = "De man van An geeft geen hand aan ambetante verwanten".toLowerCase();
-    console.log(text.toLowerCase())
-    let index = 0;
-    let atl = 0;
-    while (text.indexOf("an",index) !== -1) {
-
-        index = text.indexOf("an", index) + 2;
-        atl++;
-
-    }
-
-    console.log(atl);
 }
 
 window.addEventListener('load', setup);
